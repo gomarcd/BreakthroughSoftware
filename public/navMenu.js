aeb7b5730bb2116a795a9ff8d57dc7f8c556c194
sfritsen <sfritsen@gmail.com>
2023-05-28 23:43:24 -0600

    Updated navbar icon

--- public/navMenu.js
+++ public/navMenu.js
@@ -1,8 +1,12 @@
 const navMenu = () => {
-	var navLinks = document.getElementById("navLinks");
+	const navLinks = document.getElementById("navLinks");
+	const hamburger = document.querySelector(".hamburger");
+
 	if (navLinks.style.display === "none") {
+		hamburger.classList.toggle("active");
 		navLinks.style.display = "flex";
 	} else {
+		hamburger.classList.remove("active");
 		navLinks.style.display = "none";
 	}
 };
