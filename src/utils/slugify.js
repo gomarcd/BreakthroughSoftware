edb9e8e256b08a8448b2ce98881dcac9fed14cc1
gomarcd <git@gomarcd.dev>
2024-10-20 14:57:45 -0600

    rm unnecessary stuff

--- src/utils/slugify.js
+++
@@ -1,9 +0,0 @@
-export function slugify(text) {
-  return text
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with hyphens
-    .replace(/'/g, '') // Remove apostrophes
-    .replace(/[^\w-]+/g, '') // Remove non-word characters except hyphens
-    .replace(/--+/g, '-'); // Replace multiple consecutive hyphens with a single hyphen
-}
